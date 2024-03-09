d485ce28594e83d9e33df9bcd2125383846d5646
Emilio Dalla Torre <info@emiliodallatorre.it>
2024-03-09 13:00:42 +0100

    Added GHDL IEEE libraries

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
+export GHDL_PREFIX=$(pwd)/ghdl-macos-11-llvm/lib/ghdl
+
+echo Our GHDL prefix is now: GHDL_PREFIX=$GHDL_PREFIX
+
+echo "Compiling design..."
  ghdl -a design.vhd
+
+echo "Compiling testbench..."
  ghdl -a testbench.vhd
 
+echo "Elaborating testbench..."
  ghdl -r testbench — vcd=wave.vcd
